a5f9fbed009de130406a25af23cf72b4717f9faf
imran <zunaeede@gmail.com>
2024-04-04 22:44:06 +0600

    dom - done practice dom in typescript.

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,15 @@ const from = document.querySelector('.user-form') as HTMLFormElement;
 
 const username = document.querySelector('#name') as HTMLInputElement;
 
+const email = document.querySelector('#email') as HTMLInputElement;
+
+
 
 from.addEventListener("submit", (event: Event) => {
   event.preventDefault();
   const data = {
     username: username.value,
+    email: email.value
   };
   console.log(data);
 });
